3d42f2f15f153e0cf02f63a4fccebaab39492d9e
victor kitesh <victor.katile@student.moringaschool.com>
2021-04-11 15:28:04 +0300

    added afunction calculate

--- script.js
+++ script.js
@@ -1,2 +1,63 @@
-var bithday = parseInt(prompt("enter your birthday"));
-var gender = parseInt(prompt("enter your gender"));
+function calculate(){
+    var centuary= document.getElementById("cc").Value;
+    var centuary= document.getElementById("yy").Value;
+    var centuary= document.getElementById("mm").Value;
+    var centuary= document.getElementById("dd").Value;
+    var centuary= document.getElementById("gender").Value;
+
+
+    if(number==""||number==null){
+        Window.alert("please enter valid age!");
+        return; 
+    }
+    var data=date();
+}
+var dayoftheweek =( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+var akannamemale =[
+    {
+        sunday:"kwasi"
+    },
+    {
+        monday:"kwando"
+    },
+    {
+        tuesday:"kwabena"
+    },
+    {
+        wenesday:"kwaku"
+     },
+     {
+        thursday:"yaw"
+     },
+     {
+        friday:"kofi"
+    },
+     {
+        saturday:"kwame"
+    },
+];
+var akannamefemale =[
+    {
+        sunday:"akosua"
+    },
+    {
+        monday:"adwoa"
+    },
+    {
+        tuesday:"abenna"
+     },
+     {
+        wenesday:"akua"
+     },
+     {
+        thursday:"yaa"
+     },
+    {
+        friday:"afua"
+     },
+     {
+        saturday:"ama"
+     },  
+
+];
+
