a1829026b94e68b35468afe97f20955ed8a8669b
victor kitesh <victor.katile@student.moringaschool.com>
2021-04-11 16:20:56 +0300

    added akan name male

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
 function calculate(){
     var centuary= document.getElementById("cc").Value;
-    var centuary= document.getElementById("yy").Value;
-    var centuary= document.getElementById("mm").Value;
-    var centuary= document.getElementById("dd").Value;
-    var centuary= document.getElementById("gender").Value;
+    var year= document.getElementById("yy").Value;
+    var month= document.getElementById("mm").Value;
+    var day= document.getElementById("dd").Value;
+    var gender= document.getElementById("gender").Value;
 
+    var dayoftheweek =( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
 
-    if(number==""||number==null){
-        Window.alert("please enter valid age!");
-        return; 
+    var akannamemale ={
+    
+        sunday:"kwasi",
+    
+    
+        monday:"kwando",
+    
+
+        tuesday:"kwabena",
+    
+    
+        wenesday:"kwaku",
+     
+     
+        thursday:"yaw",
+     
+     
+        friday:"kofi",
+    
+     
+        saturday:"kwame",
+    
+};
+
+    if(day<=0||day>31){
+        alert("please enter valid day!");
+         
+         
     }
-    var data=date();
+    else if (m<= 0)||(m > 12){
+        alert("please enter valid day!");
+    }
+    else if (dayoftheweek=0&&gender=male){
+        return("akan name is"+.akannamemale[o])
+    }
+   
 }
-var dayoftheweek =( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-var akannamemale =[
-    {
-        sunday:"kwasi"
-    },
-    {
-        monday:"kwando"
-    },
-    {
-        tuesday:"kwabena"
-    },
-    {
-        wenesday:"kwaku"
-     },
-     {
-        thursday:"yaw"
-     },
-     {
-        friday:"kofi"
-    },
-     {
-        saturday:"kwame"
-    },
-];
+
+
 var akannamefemale =[
     {
         sunday:"akosua"
