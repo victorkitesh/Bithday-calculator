ccf4e0d39f9965eb21c8f0e09d2a8bec59f49cfe
victor kitesh <victor.katile@student.moringaschool.com>
2021-04-12 13:18:40 +0300

    added function getGender

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ function getBirthday() {
         return;
     }
 }
+
+
+function getGender() {
+    let gender;
+    if ((form.gender[0].checked === true) || (form.gender[1].checked === true)) {
+        if (form.gender[0].checked === true) {
+            gender = 0;
+        } else if (form.gender[1].checked === true) {
+            gender = 1;
+        }
+        return gender;
+    } else {
+        alert("Please select your gender!");
+        return;
+    }
+}
