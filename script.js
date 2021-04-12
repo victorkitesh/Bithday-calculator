aea0e3ab2026fdf3dfd85584b8b960ea13ec3e17
victor kitesh <victor.katile@student.moringaschool.com>
2021-04-12 13:13:46 +0300

    added function getBirthday

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+function getBirthday() {
+    let dateString = document.getElementById('bithday').value;
+    if (!!dateString.valueOf() === true) {
+        let bithday = new Date(dateString);
+        let d = bday.getDay();
+        return d;
+    } else {
+        alert("Please select your birthday!")
+        return;
+    }
+}
